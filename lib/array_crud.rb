e7a5c2d312b66b65e28c7acb1b30465e845b314b
Achsah Abraham <achsahmp@gmail.com>
2018-08-02 23:37:01 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-
+      mythical_creatures = ["phoenix", "wow", "am", "dragon", "wolf", "sphinx", "arrays!"]
 end
 
 def retrieve_last_element_from_array(array)
