2dfdbb8d2589eb0a8fe1163eceed6cf3df2dfb4d
Achsah Abraham <achsahmp@gmail.com>
2018-08-02 23:31:56 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   mythical_creatures = ["phoenix", "dragon", "wolf", "sphinx", "arrays!"]
-  mythical_creatures.pop("arrays!")
+  remove_arrays = mythical_creatures.pop
 end
 
 def remove_element_from_start_of_array(array)
