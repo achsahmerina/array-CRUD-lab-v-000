5b6006f345b3e21c78ebaa023ba61ba83de307c9
Achsah Abraham <achsahmp@gmail.com>
2018-08-02 23:27:24 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+  mythical_creatures = ["phoenix", "dragon", "wolf", "sphinx"]
+  mythical_creatures.push("griffin")
+  puts mythical_creatures.inspect
 end
 
 def add_element_to_start_of_array(array, element)
